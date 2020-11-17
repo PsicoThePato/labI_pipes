ec7e5006f21962ca17dc1e5069b9b8647aa3b18e
Jairo <jairo.moutinho@edu.ufes.br>
2020-11-16 22:49:58 -0300

    :construction: só falta implementar o exec e modularizar o código (opcional)

--- pipe.c
+++ pipe.c
@@ -19,7 +19,7 @@ int main()
     if (pid1 < 0)
         return 2;
         
-    if (pid1 == 0)
+    else if (pid1 == 0)
     {
         close(fd[0][1]);
         close(fd[1][0]);
@@ -39,36 +39,74 @@ int main()
         close(fd[0][0]);
 
         int array_len = (random() % 10) +1;
-        printf("%d\n", array_len);
+        printf("INFO ---- O TAMANHO DO ARRAY É: %d\n", array_len);
         int array[array_len];
-        for(int i; i <= array_len; i++)
+        
+        for(int i= 0; i < array_len; i++)
         {
             array[i] = (random() % random_x) + 1;
-            printf("%d\n", array[i]);
+            printf("O ELEMENTO %d DO ARRAY É: %d\n", i,  array[i]);
         }
+
+        write(fd[1][1], &array_len, sizeof(int));
+        write(fd[1][1], array, array_len * sizeof(int));
         close(fd[1][1]);
 
 
     }
-    
-    close(fd[0][0]);
-    close(fd[1][0]);
-    close(fd[1][1]);
-    close(fd[2][1]);
-    
-    int x;
-    scanf("%d", &x);
-    srand(5); // fixed random seed for debug purposes
-    int x_random = (random() % x) + 1;
-    write(fd[0][1], &x, sizeof(int));
 
-    char msg[] = "Meu filho, crie e envie para o seu irmão um array de números inteiros com valores randômicos entre 1 e o \
-                valor enviado anteriormente. O tamanho do array também deve ser randômico, na faixa de 1 a 10";
-    int n_caracteres = strlen(msg)+1;
-    write(fd[0][1], &n_caracteres, sizeof(int));
-    write(fd[0][1], msg, n_caracteres * sizeof(char));
-    
-    close(fd[0][1]);
-    close(fd[2][0]);
-    wait(NULL);
+    else
+    {  
+        int pid2 = fork();
+        if (pid2 < 0)
+            return 3;
+        if (pid2 == 0)
+        {
+            close(fd[0][0]);
+            close(fd[0][1]);
+            close(fd[1][1]);
+            close(fd[2][0]);
+
+            int array_len;
+            read(fd[1][0], &array_len, sizeof(int));
+            int array[array_len];
+            read(fd[1][0], array, array_len*sizeof(int));
+            close(fd[1][0]);
+
+            int sum = 0;
+            for(int i = 0; i < array_len; i++)
+            {
+                sum += array[i];
+            }
+
+            write(fd[2][1], &sum, sizeof(int));
+        }
+        else
+        {
+            close(fd[0][0]);
+            close(fd[1][0]);
+            close(fd[1][1]);
+            close(fd[2][1]);
+            
+            int x;
+            scanf("%d", &x);
+            srand(5); // fixed random seed for debug purposes
+            int x_random = (random() % x) + 1;
+            write(fd[0][1], &x, sizeof(int));
+
+            char msg[] = "Meu filho, crie e envie para o seu irmão um array de números inteiros com valores randômicos entre 1 e o \
+                        valor enviado anteriormente. O tamanho do array também deve ser randômico, na faixa de 1 a 10";
+            int n_caracteres = strlen(msg)+1;
+            write(fd[0][1], &n_caracteres, sizeof(int));
+            write(fd[0][1], msg, n_caracteres * sizeof(char));
+            
+            int sum;
+            read(fd[2][0], &sum, sizeof(int));
+            printf("A soma é: %d\n", sum);
+            
+            wait(NULL);
+            close(fd[0][1]);
+            close(fd[2][0]);
+        }
+    } 
 }
